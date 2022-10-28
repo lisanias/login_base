df6ea6a3e00820c41a416a5cf61af01c63534154
Lisanias Loback <pastorlisanias@gmail.com>
2022-10-27 21:18:03 -0300

    adionar pasta separa para teste de email /email

--- email/index.php
+++ email/index.php
@@ -6,6 +6,8 @@ use Source\Support\Email;
 
 $email = new Email();
 
+die("terminou");
+
 $email->add(
     subject: "Email enviado por webig.pro.br",
     body: "<h1>Email de Teste</h1>Vamos ver se chegou e se deu certo",
